b77edf5487d98526043ff0b4f22236e090efec6a
Henintsoa Paul <henintsoapaul@gmail.com>
2024-11-05 20:42:58 +0200

    [api][servlet]-Ajout BlocServlet
    * Recup data depuis WebService
    * Creation instance Bloc
    * Controle -> Insertion

---
+++ KidoroAPI/kidoro-war/src/java/BlocServlet.java
@@ -0,0 +1,41 @@
+import cube.bloc.Bloc;
+import cube.bloc.MyBloc;
+import utils.EJBGetter;
+import utils.json.JsonError;
+import utils.json.JsonUtil;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet( "blocs" )
+public class BlocServlet extends HeninServlet {
+
+    @Override
+    protected void doPost( HttpServletRequest req, HttpServletResponse resp )
+            throws IOException {
+        try {
+            super.setCORS( resp );
+            String json = JsonUtil.getFromWeb( req );
+            super.dataOk( resp );
+
+            System.out.println( json );
+            MyBloc myBloc = gson.fromJson( json, MyBloc.class );
+            Bloc bloc = myBloc.creerBloc();
+
+            EJBGetter.getBlocEJB().add( bloc );
+        } catch ( Exception e ) {
+            resp.setStatus( HttpServletResponse.SC_INTERNAL_SERVER_ERROR );
+            resp.getWriter().write( gson.toJson( new JsonError( "Erreur interne" ) ) );
+
+            throw new RuntimeException( e );
+        }
+    }
+
+    @Override
+    protected void doOptions( HttpServletRequest req, HttpServletResponse resp ) {
+        setCORS( resp );
+        resp.setStatus( HttpServletResponse.SC_OK );
+    }
+}
