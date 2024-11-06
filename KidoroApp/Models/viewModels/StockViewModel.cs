01c0084e5ed9af34a6d58448f9c327c8ac81dd3b
Henintsoa Paul <henintsoapaul@gmail.com>
2024-11-06 16:32:53 +0200

    [aff]-Afficher total des stocks

--- KidoroApp/Models/viewModels/StockViewModel.cs
+++ KidoroApp/Models/viewModels/StockViewModel.cs
@@ -6,4 +6,35 @@ public class StockViewModel(List<StockUsuel> usuels, List<StockBloc> blocs)
 {
     public List<StockUsuel> Usuels { get; set; } = usuels;
     public List<StockBloc> Blocs { get; set; } = blocs;
+
+    // Methods
+    // Usuels
+    public int GetTotalQuantiteUsuels()
+    {
+        return Usuels.Sum(u => u.quantite);
+    }
+
+    public decimal GetTotalPrixVenteUsuels()
+    {
+        return Usuels.Sum(u => u.sumPrixVente);
+    }
+
+    public decimal GetTotalPrixRevientUsuels()
+    {
+        return Usuels.Sum(u => u.sumPrixRevient);
+    }
+
+    // Blocs
+    public decimal GetTotalPrixRevientBloc()
+    {
+        return Blocs.Sum(b => b.prixRevient);
+    }
+    public decimal GetTotalPrixVenteOptimisteBloc()
+    {
+        return Blocs.Sum(b => b.prixVenteOptimiste);
+    }
+    public decimal GetTotalPrixVenteVolMinBloc()
+    {
+        return Blocs.Sum(b => b.prixVenteVolMin);
+    }
 }
