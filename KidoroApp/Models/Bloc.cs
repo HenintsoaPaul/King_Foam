f0c192efec07a3fb5206eecb83a77adca4d74c34
Henintsoa Paul <henintsoapaul@gmail.com>
2024-11-06 20:00:40 +0200

    [app][model]-Ajouter class Cube

--- KidoroApp/Models/Bloc.cs
+++ KidoroApp/Models/Bloc.cs
@@ -2,14 +2,11 @@
 
 namespace KidoroApp.Models;
 
-public class Bloc : ClassMapTable
+public class Bloc : Cube
 {
     public string id { get; set; }
     public string id_bloc_mere { get; set; }
     public string daty_entree { get; set; }
     public string daty_sortie { get; set; }
     public double prix_revient { get; set; }
-    public double longueur { get; set; }
-    public double largeur { get; set; }
-    public double hauteur { get; set; }
 }
