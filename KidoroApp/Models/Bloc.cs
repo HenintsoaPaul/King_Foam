be107a0c8febd8ba3196f520c6924918ef35aea7
Henintsoa Paul <henintsoapaul@gmail.com>
2024-11-06 09:01:55 +0200

    [webService][app]-Get all blocs, all usuel

---
+++ KidoroApp/Models/Bloc.cs
@@ -0,0 +1,15 @@
+﻿using Boutik.Models;
+
+namespace KidoroApp.Models;
+
+public class Bloc : ClassMapTable
+{
+    public string id { get; set; }
+    public string id_bloc_mere { get; set; }
+    public string daty_entree { get; set; }
+    public string daty_sortie { get; set; }
+    public double prix_revient { get; set; }
+    public double longueur { get; set; }
+    public double largeur { get; set; }
+    public double hauteur { get; set; }
+}
---
+++ KidoroApp/Models/Usuel.cs
@@ -0,0 +1,14 @@
+﻿using Boutik.Models;
+
+namespace KidoroApp.Models;
+
+public class Usuel : ClassMapTable
+{
+    public string id { get; set; }
+    public string val { get; set; }
+    public double prix_revient { get; set; }
+    public double prix_vente { get; set; }
+    public double longueur { get; set; }
+    public double largeur { get; set; }
+    public double hauteur { get; set; }
+}
