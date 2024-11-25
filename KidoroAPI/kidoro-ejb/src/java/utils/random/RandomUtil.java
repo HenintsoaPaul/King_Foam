dc30259ff7f0061b7e09d8640e84e3475bc94c20
Henintsoa Paul <henintsoapaul@gmail.com>
2024-11-25 07:51:32 +0200

    [metier]-random bloc * date * dimension * pr

--- KidoroAPI/kidoro-ejb/src/java/utils/random/RandomUtil.java
+++ KidoroAPI/kidoro-ejb/src/java/utils/random/RandomUtil.java
@@ -1,6 +1,14 @@
 package utils.random;
 
+import bean.CGenUtil;
 import cube.Cube;
+import cube.bloc.Bloc;
+import holiday.Holiday;
+import utils.DateUtil;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public abstract class RandomUtil {
     protected Number minLong = 20, maxLong = 25;
@@ -13,5 +21,54 @@ public abstract class RandomUtil {
         cube.setLongueur( getRand( minLong, maxLong ) );
         cube.setLargeur( getRand( minLarg, maxLarg ) );
         cube.setHauteur( getRand( minHaut, maxHaut ) );
+    }
+
+    public Date getRandomDate( Date start, Date end, List<Holiday> holidayList ) {
+        // Generate random number of days within the interval
+        long randomDays = ThreadLocalRandom.current().nextLong( 0, ( end.getTime() - start.getTime() ) / ( 1000 * 60 * 60 * 24 ) + 1 );
+        long randomDateInMillis = start.getTime() + randomDays * ( 1000 * 60 * 60 * 24 );
+
+        // Ensure the generated date doesn't exceed the end date
+        if ( randomDateInMillis > end.getTime() ) {
+            randomDateInMillis = end.getTime();
+        }
+
+        while ( DateUtil.isNotJrOuvrable( holidayList, new Date( randomDateInMillis ) ) ) {
+            // If it's a holiday, generate another random date
+            randomDays = ThreadLocalRandom.current().nextLong( 0, ( end.getTime() - start.getTime() ) / ( 1000 * 60 * 60 * 24 ) + 1 );
+            randomDateInMillis = start.getTime() + randomDays * ( 1000 * 60 * 60 * 24 );
+
+            // Ensure we don't exceed the end date
+            if ( randomDateInMillis > end.getTime() ) {
+                randomDateInMillis = end.getTime();
+            }
+        }
+
+        return new Date( randomDateInMillis );
+    }
+
+    public Bloc getRandomBloc( Date start, Date end, List<Holiday> holidayList ) {
+        Bloc bloc = new Bloc();
+        bloc.setDaty_entree( getRandomDate( start, end, holidayList ) );
+        this.setRandDimensions( bloc );
+        return bloc;
+    }
+
+    public double getPrAvecVariation( double pRevient, double marge ) {
+        double max = pRevient + ( pRevient * ( marge / 100 ) ),
+                min = pRevient - ( pRevient * ( marge / 100 ) );
+        return RandomDoubleUtil.getRandDouble( min, max );
+    }
+
+    public double getMoyennePr()
+            throws Exception {
+        String apresWhere = "";
+        Bloc[] blocs = ( Bloc[] ) CGenUtil.rechercher( new Bloc(), null, null, apresWhere );
+        double r = 0, vols = 0;
+        for ( Bloc b:  blocs ) {
+            r += b.getPrixRevientVolumique();
+            vols += b.getVolume();
+        }
+        return r / vols;
     }
 }
--- KidoroAPI/kidoro-war/src/java/RandomServlet.java
+++ KidoroAPI/kidoro-war/src/java/RandomServlet.java
@@ -1,12 +1,14 @@
 import cube.bloc.Bloc;
+import holiday.Holiday;
+import utils.EJBGetter;
 import utils.random.RandomIntUtil;
-import utils.random.RandomUtil;
 import utils.json.JsonError;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,14 +22,27 @@ public class RandomServlet extends HeninServlet {
             resp.setContentType( "application/json" );
             super.setCORS( resp );
 
+            RandomIntUtil randomIntUtil = new RandomIntUtil();
+            Date startDate = Date.valueOf( "2022-01-01" );
+            Date endDate = Date.valueOf( "2024-12-31" );
+
+            List<Bloc> blocs = new ArrayList<>();
+            List<Holiday> holidayList = EJBGetter.getHolidayEJB().getAll();
+
+            double moyennePr = randomIntUtil.getMoyennePr();
+
+            double prRevientVolumique = 500,
+                    marge = 10,
+                    prRehetra = randomIntUtil.getPrAvecVariation( prRevientVolumique, marge );
+
             Bloc b;
-            List<Bloc> l = new ArrayList<Bloc>();
-            for ( int i = 0; i < 3; i++ ) {
-                b = new Bloc();
-                new RandomIntUtil().setRandDimensions( b );
-                l.add( b );
+            for ( int i = 0; i < 2; i++ ) {
+                b = randomIntUtil.getRandomBloc( startDate, endDate, holidayList );
+                b.setPrix_revient_pratique( prRehetra * b.getVolume() );
+                blocs.add( b );
             }
-            resp.getWriter().println( gson.toJson( l ) );
+
+            resp.getWriter().println( gson.toJson( blocs ) );
         } catch ( Exception e ) {
             e.printStackTrace();
             resp.setStatus( HttpServletResponse.SC_INTERNAL_SERVER_ERROR );
